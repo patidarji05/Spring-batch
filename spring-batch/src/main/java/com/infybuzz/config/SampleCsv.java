e476a33b6dfef02cbb574051dc8b29a40ba7351c
patidarji05 <pradeeppatidar9009@gmail.com>
2024-08-28 08:29:12 +0530

    add jobparameter

--- spring-batch/src/main/java/com/infybuzz/config/SampleCsv.java
+++ spring-batch/src/main/java/com/infybuzz/config/SampleCsv.java
@@ -7,6 +7,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
@@ -14,14 +15,18 @@ import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 
 import com.infybuzz.model.StudentCsv;
 
+/*
+ * This  config file is used to read data from CSV file 
+ */
 @Configuration
-public class SampleOne {
+public class SampleCsv {
 
 	@Autowired
 	JobBuilderFactory jobBuilderFactory;
@@ -37,22 +42,31 @@ public class SampleOne {
 	}
 
 	public Step firstChunkStep() {
-		return stepBuilderFactory.get("First Chunk Step").<StudentCsv, StudentCsv>chunk(3).reader(flateFileItemReader())
+		return stepBuilderFactory.get("First Chunk Step").<StudentCsv, StudentCsv>chunk(3)
+				.reader(flateFileItemReader(null))
 				// .processor(firstItemProcesser)
 				.writer(itemWriter()).build();
 	}
 
-	public FlatFileItemReader<StudentCsv> flateFileItemReader() {
+	// public FlatFileItemReader<StudentCsv> flateFileItemReader() {
+	
+	// read value from jobparameter
+	@StepScope
+	@Bean
+	public FlatFileItemReader<StudentCsv> flateFileItemReader(@Value("#{jobParameters['inputFile']}") FileSystemResource fileSystemResource) {
 		FlatFileItemReader<StudentCsv> flatFileItemReader = new FlatFileItemReader<StudentCsv>();
 
-		flatFileItemReader.setResource(new FileSystemResource(
-				new File("C:\\Users\\prade\\git\\spring-batch\\spring-batch\\InputFiles\\student.csv")));
+//		flatFileItemReader.setResource(new FileSystemResource(
+//				new File("C:\\Users\\prade\\git\\spring-batch\\spring-batch\\InputFiles\\student.csv")));
 
+		flatFileItemReader.setResource(fileSystemResource);
+		
 		flatFileItemReader.setLineMapper(new DefaultLineMapper<StudentCsv>() {
 			{
-				setLineTokenizer(new DelimitedLineTokenizer() {
+				setLineTokenizer(new DelimitedLineTokenizer("|") {
 					{
 						setNames("ID", "First Name", "Last Name", "Email");
+					//	setDelimiter("|");
 					}
 				});
 
