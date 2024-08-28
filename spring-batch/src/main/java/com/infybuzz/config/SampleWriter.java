d09157e9292abee437630cdf6de30396beba8456
patidarji05 <pradeeppatidar9009@gmail.com>
2024-08-28 14:20:41 +0530

    write data inside the csv and json file

---
+++ spring-batch/src/main/java/com/infybuzz/config/SampleWriter.java
@@ -0,0 +1,134 @@
+package com.infybuzz.config;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Date;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.file.FlatFileFooterCallback;
+import org.springframework.batch.item.file.FlatFileHeaderCallback;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.batch.item.json.JacksonJsonObjectMarshaller;
+import org.springframework.batch.item.json.JsonFileItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import com.infybuzz.model.StudentJdbc;
+
+@Configuration
+public class SampleWriter {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Autowired
+	@Qualifier("dataSource")
+	private DataSource dataSource;
+
+	@Bean
+	public Job chunkJob() {
+
+		return jobBuilderFactory.get("Chunk Job")
+				.incrementer(new RunIdIncrementer())
+				.start(firstChunkStep()).build();
+
+	}
+	
+	public Step firstChunkStep() {
+		return stepBuilderFactory.get("First Chunk Step").<StudentJdbc, StudentJdbc>chunk(3)
+				
+				.reader(jdbcCursorItemReader())
+				// .processor(firstItemProcesser)
+				//.writer(flatFileItemWriter(null))
+				.writer(jsonFileItemWriter(null))
+				.build();
+				
+	}
+	
+	public JdbcCursorItemReader<StudentJdbc> jdbcCursorItemReader() {
+		JdbcCursorItemReader<StudentJdbc> jdbcCursorItemReader = new JdbcCursorItemReader<>();
+
+		jdbcCursorItemReader.setDataSource(dataSource);
+		jdbcCursorItemReader.setSql("select id, first_name as firstName, last_name as lastName, email from student");
+
+		jdbcCursorItemReader.setRowMapper(new BeanPropertyRowMapper<StudentJdbc>() {
+
+			{
+				setMappedClass(StudentJdbc.class);
+			}
+
+		});
+
+		return jdbcCursorItemReader;
+	}
+	
+	@StepScope
+	@Bean
+	public FlatFileItemWriter<StudentJdbc> flatFileItemWriter(
+			@Value("#{jobParameters['outputFile']}") FileSystemResource fileSystemResource){
+		FlatFileItemWriter<StudentJdbc> flatFileItemWriter = new FlatFileItemWriter<>();
+		
+		flatFileItemWriter.setResource(fileSystemResource);
+		flatFileItemWriter.setHeaderCallback(new FlatFileHeaderCallback() {
+			
+			@Override
+			public void writeHeader(Writer writer) throws IOException {
+				writer.write("Id,First Name, Last Name,email");
+				
+			}
+		});
+		
+		
+		flatFileItemWriter.setLineAggregator(new DelimitedLineAggregator<StudentJdbc>() {
+			{
+				setFieldExtractor(new BeanWrapperFieldExtractor<StudentJdbc>() {
+					{
+						setNames(new String[] {"id","firstName","lastName","email"});
+					}
+				});
+			}
+		});
+		
+		flatFileItemWriter.setFooterCallback(new FlatFileFooterCallback() {
+			
+			@Override
+			public void writeFooter(Writer writer) throws IOException {
+				
+				writer.write("created @ "+ new Date());
+			}
+		});
+		
+		return flatFileItemWriter;
+	}
+	
+	@StepScope
+	@Bean
+	public JsonFileItemWriter<StudentJdbc>  jsonFileItemWriter(
+			@Value("#{jobParameters['outputFile']}") FileSystemResource fileSystemResource){
+		
+		JsonFileItemWriter<StudentJdbc>  jsonFileItemWriter = new JsonFileItemWriter<>(fileSystemResource, 
+          new JacksonJsonObjectMarshaller<StudentJdbc>());
+		
+		return jsonFileItemWriter;
+		
+	}
+
+}
