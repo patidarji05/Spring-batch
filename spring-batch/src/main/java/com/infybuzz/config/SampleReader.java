ed06d53971f9b4a8e768807c5b9d80b497fcd111
patidarji05 <pradeeppatidar9009@gmail.com>
2024-08-28 12:11:57 +0530

    read data from db

--- spring-batch/src/main/java/com/infybuzz/config/SampleReader.java
+++ spring-batch/src/main/java/com/infybuzz/config/SampleReader.java
@@ -1,7 +1,8 @@
 package com.infybuzz.config;
 
-import java.io.File;
 import java.util.List;
+
+import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -10,6 +11,7 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -19,54 +21,81 @@ import org.springframework.batch.item.json.JsonItemReader;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import com.infybuzz.model.StudentCsv;
+import com.infybuzz.model.StudentJdbc;
 import com.infybuzz.model.StudentJson;
 import com.infybuzz.model.StudentXml;
 
 /*
- * This  config file is used to read data from CSV , Json, xml file 
+ * This  config file is used to read data from CSV , Json, xml, db file 
  */
 @Configuration
 public class SampleReader {
 
 	@Autowired
-	JobBuilderFactory jobBuilderFactory;
+	private JobBuilderFactory jobBuilderFactory;
 
 	@Autowired
-	StepBuilderFactory stepBuilderFactory;
+	private StepBuilderFactory stepBuilderFactory;
+
+	 @Autowired
+	 private DataSource dataSource;
+
+	
 
 	@Bean
 	public Job chunkJob() {
 
-		return jobBuilderFactory.get("Chunk Job")
-				.incrementer(new RunIdIncrementer())
-				.start(firstChunkStep()).build();
+		return jobBuilderFactory.get("Chunk Job").incrementer(new RunIdIncrementer()).start(firstChunkStep()).build();
 
 	}
 
 	public Step firstChunkStep() {
-		return stepBuilderFactory.get("First Chunk Step")
-				.<StudentXml, StudentXml>chunk(3)
-			//	.reader(flateFileItemReader(null))
-				//.reader(jsonItemReader(null))
-				.reader(staxEventItemReader(null))
+		return stepBuilderFactory.get("First Chunk Step").<StudentJdbc, StudentJdbc>chunk(3)
+				// .reader(flateFileItemReader(null))
+				// .reader(jsonItemReader(null))
+				// .reader(staxEventItemReader(null))
+				.reader(jdbcCursorItemReader())
 				// .processor(firstItemProcesser)
 				.writer(itemWriter()).build();
 	}
 
-	
-	
-	// read the data form xml file 
-	
+	// read jaba from db
+
+	public JdbcCursorItemReader<StudentJdbc> jdbcCursorItemReader() {
+		JdbcCursorItemReader<StudentJdbc> jdbcCursorItemReader = new JdbcCursorItemReader<>();
+
+		jdbcCursorItemReader.setDataSource(dataSource);
+		jdbcCursorItemReader.setSql("select id, first_name as firstName, last_name as lastName, email from student");
+
+		jdbcCursorItemReader.setRowMapper(new BeanPropertyRowMapper<StudentJdbc>() {
+
+			{
+				setMappedClass(StudentJdbc.class);
+			}
+
+		});
+
+		jdbcCursorItemReader.setCurrentItemCount(2);
+		jdbcCursorItemReader.setMaxItemCount(8);
+		return jdbcCursorItemReader;
+	}
+
+	// read the data form xml file
+
 	@StepScope
-	@Bean
-	public StaxEventItemReader<StudentXml> staxEventItemReader(@Value("#{jobParameters['inputFile']}") FileSystemResource fileSystemResource){
-		
+	// @Bean
+	public StaxEventItemReader<StudentXml> staxEventItemReader(
+			@Value("#{jobParameters['inputFile']}") FileSystemResource fileSystemResource) {
+
 		StaxEventItemReader<StudentXml> staxEventItemReader = new StaxEventItemReader<>();
 		staxEventItemReader.setResource(fileSystemResource);
 		staxEventItemReader.setFragmentRootElementName("student");
@@ -75,34 +104,30 @@ public class SampleReader {
 				setClassesToBeBound(StudentXml.class);
 			}
 		});
-		
+
 		return staxEventItemReader;
 	}
-	
-	
-	
-	
-	
-	// read tha data from json file 
+
+	// read tha data from json file
 	@StepScope
 	@Bean
-	public JsonItemReader<StudentJson> jsonItemReader(@Value("#{jobParameters['inputFile']}") FileSystemResource fileSystemResource){
-		
+	public JsonItemReader<StudentJson> jsonItemReader(
+			@Value("#{jobParameters['inputFile']}") FileSystemResource fileSystemResource) {
+
 		JsonItemReader<StudentJson> jsonItemReader = new JsonItemReader<>();
 		jsonItemReader.setResource(fileSystemResource);
-		
+
 		jsonItemReader.setJsonObjectReader(new JacksonJsonObjectReader<>(StudentJson.class));
-		
+
 		jsonItemReader.setMaxItemCount(8);
 		jsonItemReader.setCurrentItemCount(2);
-		
+
 		return jsonItemReader;
-		
+
 	}
-	
-	
+
 	// read value from jobparameter
-	
+
 	// public FlatFileItemReader<StudentCsv> flateFileItemReader() {
 
 	@StepScope
@@ -138,24 +163,24 @@ public class SampleReader {
 		delimitedLineTokenizer.setNames("ID", "First Name", "Last Name", "Email");
 
 		defaultLineMapper.setLineTokenizer(delimitedLineTokenizer);
-		
+
 		BeanWrapperFieldSetMapper<StudentCsv> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
 		fieldSetMapper.setTargetType(StudentCsv.class);
-		
+
 		defaultLineMapper.setFieldSetMapper(fieldSetMapper);
-		
+
 		flatFileItemReader.setLineMapper(defaultLineMapper);
 
 		flatFileItemReader.setLinesToSkip(1);
 		return flatFileItemReader;
 	}
 
-	public ItemWriter<StudentXml> itemWriter() {
+	public ItemWriter<StudentJdbc> itemWriter() {
 
-		return new ItemWriter<StudentXml>() {
+		return new ItemWriter<StudentJdbc>() {
 
 			@Override
-			public void write(List<? extends StudentXml> items) throws Exception {
+			public void write(List<? extends StudentJdbc> items) throws Exception {
 				System.out.println("Inside the item reader");
 				items.stream().forEach(System.out::println);
 
